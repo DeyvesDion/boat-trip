74abb10807ccfbc7d8e231e195a654de8ffc42ec
DeyvesDion <deyvesdion@gmail.com>
2020-09-04 19:20:40 +0200

    add image in the store

--- src/components/featuredBoat/FeaturedBoat.vue
+++ src/components/featuredBoat/FeaturedBoat.vue
@@ -5,32 +5,33 @@
                 Les plus populaire
             </h2>
 
-            <b-row class="featuredBoatCard">
-                <b-col
-                    sm="6"
-                    md="4"
-                    lg="4"
-                    xl="3"
+            <div class="row">
+                <div
+                    class="col-sm-6 col-md-4 col-lg-4 col-xl-3"
                     v-for="(boat, index) in boats"
                     :key="index"
                 >
-                    <b-card
-                        img-src="https://picsum.photos/600/300/?image=25"
-                        img-alt="Image"
-                        img-top
-                        tag="article"
+                    <div
+                        class="card mb-2"
                         style="max-width: 50rem; cursor:pointer"
-                        class="mb-2"
                     >
-                        <h4 class="mb-2">{{ boat.name }}</h4>
-                        <b-card-text>
-                            {{ boat.description }}
-                        </b-card-text>
-
-                        <b-button class="btnLocation" href="#">Louer</b-button>
-                    </b-card>
-                </b-col>
-            </b-row>
+                        <img
+                            class="card-img-top"
+                            :src="boat.imgUrl"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <h5 class="card-title">{{ boat.name }}</h5>
+                            <p class="card-text">
+                                {{ boat.description }}
+                            </p>
+                            <b-button class="btnLocation" href="#"
+                                >Louer</b-button
+                            >
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
 </template>
--- src/components/featuredDestinations/FeaturedDestinations.vue
+++ src/components/featuredDestinations/FeaturedDestinations.vue
@@ -24,25 +24,35 @@
             <h2 class="subTitle">
                 Destinations coup de coeur
             </h2>
-            <b-row class="featuredDestinationsLitsCard">
-                <b-col sm="6" md="4" lg="4" xl="3">
-                    <b-card
-                        img-src="https://picsum.photos/600/300/?image=25"
-                        img-alt="Image"
-                        img-top
-                        tag="article"
+            <div class="row">
+                <div
+                    class="col-sm-6 col-md-4 col-lg-4 col-xl-3"
+                    v-for="(destination, index) in destinations"
+                    :key="index"
+                >
+                    <div
+                        class="card mb-2"
                         style="max-width: 50rem; cursor:pointer"
-                        class="mb-2"
                     >
-                        <h4 class="mb-2">kjjjfj jffh</h4>
-                        <b-card-text>
-                            lorerv jiefijfiji jjijij jojj joji joj
-                        </b-card-text>
-
-                        <b-button class="btnLocation" href="#">Louer</b-button>
-                    </b-card>
-                </b-col>
-            </b-row>
+                        <img
+                            class="card-img-top"
+                            :src="destination.imgUrl"
+                            alt="Card image cap"
+                        />
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{ destination.name }}
+                            </h5>
+                            <p class="card-text">
+                                {{ destination.description }}
+                            </p>
+                            <b-button class="btnLocation" href="#"
+                                >Visiter {{ destination.name }}
+                            </b-button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -50,6 +60,11 @@
 <script>
 export default {
     name: "FeaturedDestinations",
+    computed: {
+        destinations() {
+            return this.$store.state.destinations;
+        },
+    },
 };
 </script>
 
